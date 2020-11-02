dede8596447c9cefaaf17c82e2ce26bf0cfe8034
Tomas Padilla <padillatomasagustin@gmail.com>
2020-11-02 16:29:24 +0100

    starting

---
+++ src/Awwwards/AwwwardsApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+const AwwwardsApp = () => {
+  return (
+    <React.Fragment>
+      <Header></Header>
+    </React.Fragment>
+  );
+};
+
+export default AwwwardsApp;
---
+++ src/Awwwards/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header className='header'>
+        <div className='header-container'>
+          <div className='header-row'>
+            <div className='logo'>
+              <h1>Agency</h1>
+            </div>
+            <nav className='navbar'>
+              <span></span>
+              <span></span>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </React.Fragment>
+  );
+};
+
+export default Header;
